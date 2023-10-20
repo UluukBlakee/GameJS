7b3f539a744f099892da90d22f838148d842c127
Uluk <talantbuluk@gmail.com>
2023-10-20 12:14:11 +0600

    Added dragon move

--- script.js
+++ script.js
@@ -20,28 +20,27 @@ let dragon = {
 
 let scoreboard = $('#scoreboard');
 let count = 1;
+let choice = $('#choice');
 
 function heroAttack() {
-    count++;
-    scoreboard.text(count);
-
     let attackChance = getRandomInt(4);
     let heroStrength = parseInt(hero.strength);
     let heroWeapon = parseInt(hero.weapon);
     let dragonDefence = parseInt(dragon.defence);
-    let damage = heroStrength + heroWeapon - dragonDefence;
+    let damageHero = heroStrength + heroWeapon - dragonDefence;
 
     if (attackChance == 0){
         let heroMessage = $('<div class="card"><div class="card-header"><span class="fw-bold">Герой</span></div><div class="card-body">Герой не попал по дракону</div><div class="card-footer"></div></div>');  
         $('.message #hero-message').append(heroMessage);
     }
     else {
-        dragon.hp -= damage;
+        dragon.hp -= damageHero;
         let heroMessage = $('<div class="card"><div class="card-header"><span class="fw-bold">Герой</span></div><div class="card-body">Герой нанес урон по дракону</div><div class="card-footer"></div></div>');
-        heroMessage.find('.card-footer').text(damage);
+        heroMessage.find('.card-footer').text('Нанесенный урон: ' + damageHero);
         $('.dragon #HP').text(dragon.hp);
         $('.message #hero-message').append(heroMessage);
     }
+
     if(dragon.hp <= 0 ){
         $('.container').remove()
         let newDiv = $('<div class="item">Поздравляем, вы выиграли</div>');
@@ -49,10 +48,45 @@ function heroAttack() {
     }
 }
 
-    $('#Attack').on('click', function(event) {
+function dragonAttack() {
+    let attackChance = getRandomInt(2);
+    let dragStrength = parseInt(dragon.strength);
+    let dragWeapon = parseInt(dragon.weapon);
+    let heroDefence = parseInt(hero.defence);
+    let damageDrag = dragStrength + dragWeapon - heroDefence;
+
+    if (attackChance == 0){
+        let dragMessage = $('<div class="card"><div class="card-header"><span class="fw-bold">Дракон</span></div><div class="card-body">Дракон не стал атаковать героя</div><div class="card-footer"></div></div>');  
+        $('.message #dragon-message').append(dragMessage);
+    }
+    else {
+        hero.hp -= damageDrag;
+        let dragMessage = $('<div class="card"><div class="card-header"><span class="fw-bold">Дракон</span></div><div class="card-body">Дракон нанес урон по герою</div><div class="card-footer"></div></div>');
+        dragMessage.find('.card-footer').text('Нанесенный урон: ' + damageDrag);
+        $('.hero #HP').text(hero.hp);
+        $('.message #dragon-message').append(dragMessage);
+    }
+
+    if(hero.hp <= 0 ){
+        $('.container').remove()
+        let newDiv = $('<div class="item">К сожаления вы проиграли</div>');
+        $('body').append(newDiv);
+    }
+}
+
+    choice.on('click', function(event) {
     event.preventDefault();
+    scoreboard.text(count++);
     $('.message #hero-message .card').remove();
-    heroAttack();
+    $('.message #dragon-message .card').remove();
+    let selectedAction = $('.form-select').val();
+    if (selectedAction == 'Attack'){
+        heroAttack();
+    }
+    else if (selectedAction == 'Defence'){
+
+    }
+    dragonAttack();
 });
 
 
