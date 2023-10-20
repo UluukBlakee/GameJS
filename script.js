59377096125efd778d173116a693a34ec81fa882
Uluk <talantbuluk@gmail.com>
2023-10-20 13:06:05 +0600

    Added the ability to choose what the hero will do

--- script.js
+++ script.js
@@ -3,20 +3,26 @@ function getRandomInt(max) {
 }
 
 let hero = {
-    hp: $('.hero #HP').text(),
-    strength:  $('.hero #Strength').text(),
-    defence:  $('.hero #Defence').text(),
-    weapon:  $('.hero #Weapon').text(),
-    shield:  $('.hero #Shield').text()
+    hp: parseInt($('.hero #HP').text()),
+    strength: parseInt($('.hero #Strength').text()),
+    defence: parseInt($('.hero #Defence').text()),
+    weapon: parseInt($('.hero #Weapon').text()),
+    shield:  parseInt($('.hero #Shield').text())
 };
 
 let dragon = {
-    hp: $('.dragon #HP').text(),
-    strength:  $('.dragon #Strength').text(),
-    defence:  $('.dragon #Defence').text(),
-    weapon:  $('.dragon #Weapon').text(),
-    shield:  $('.dragon #Shield').text()
+    hp: parseInt($('.dragon #HP').text()),
+    strength: parseInt($('.dragon #Strength').text()),
+    defence: parseInt($('.dragon #Defence').text()),
+    weapon: parseInt($('.dragon #Weapon').text())
 };
+
+let hellhound = {
+    hp: 750,
+    strength: 250,
+    defence: 50,
+    weapon: 100
+}
 
 let scoreboard = $('#scoreboard');
 let count = 1;
@@ -24,10 +30,7 @@ let choice = $('#choice');
 
 function heroAttack() {
     let attackChance = getRandomInt(4);
-    let heroStrength = parseInt(hero.strength);
-    let heroWeapon = parseInt(hero.weapon);
-    let dragonDefence = parseInt(dragon.defence);
-    let damageHero = heroStrength + heroWeapon - dragonDefence;
+    let damageHero = hero.strength + hero.weapon - dragon.defence;
 
     if (attackChance == 0){
         let heroMessage = $('<div class="card"><div class="card-header"><span class="fw-bold">Герой</span></div><div class="card-body">Герой не попал по дракону</div><div class="card-footer"></div></div>');  
@@ -48,12 +51,17 @@ function heroAttack() {
     }
 }
 
+function heroDefence() {
+    hero.defence += hero.shield;
+    let heroMessage = $('<div class="card"><div class="card-header"><span class="fw-bold">Герой</span></div><div class="card-body">Герой поднял щит</div><div class="card-footer"></div></div>');
+    heroMessage.find('.card-footer').text('Защита увеличилось на: ' + hero.shield);
+    $('.hero #Defence').text(hero.defence);
+    $('.message #hero-message').append(heroMessage);
+}
+
 function dragonAttack() {
     let attackChance = getRandomInt(2);
-    let dragStrength = parseInt(dragon.strength);
-    let dragWeapon = parseInt(dragon.weapon);
-    let heroDefence = parseInt(hero.defence);
-    let damageDrag = dragStrength + dragWeapon - heroDefence;
+    let damageDrag = dragon.strength + dragon.weapon - hero.defence;
 
     if (attackChance == 0){
         let dragMessage = $('<div class="card"><div class="card-header"><span class="fw-bold">Дракон</span></div><div class="card-body">Дракон не стал атаковать героя</div><div class="card-footer"></div></div>');  
@@ -79,14 +87,28 @@ function dragonAttack() {
     scoreboard.text(count++);
     $('.message #hero-message .card').remove();
     $('.message #dragon-message .card').remove();
+    if (hero.defence != 50){
+        hero.defence -= hero.shield;
+        $('.hero #Defence').text(hero.defence);
+    }
     let selectedAction = $('.form-select').val();
     if (selectedAction == 'Attack'){
         heroAttack();
     }
     else if (selectedAction == 'Defence'){
+        heroDefence();
+    }
+    else{
+        let heroMessage = $('<div class="card"><div class="card-header"><span class="fw-bold">Герой</span></div><div class="card-body">Герой не стал атаковать дракона</div><div class="card-footer"></div></div>');  
+        $('.message #hero-message').append(heroMessage);
+    }
+    let choosingComp = getRandomInt(2);
+    if (choosingComp == 0){
+        dragonAttack();
+    }
+    else {
 
     }
-    dragonAttack();
 });
 
 
